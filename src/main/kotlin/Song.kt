7a52c95ef7293378be35d5835dc1ac538ba7a503
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2018-10-04 10:35:59 -0600

    Clase song

--- src/main/kotlin/Song.kt
+++ src/main/kotlin/Song.kt
@@ -1,3 +1,37 @@
-data class Song(
+import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.google.gson.Gson
 
-)
+data class Song(
+        val year: Int,
+        val country: String,
+        val region: String,
+        val artistName: String,
+        val song: String,
+        val artistGender: String,
+        val groupOrSolo: String,
+        val place: Int,
+        val points: Int,
+        val isFinal: Boolean,
+        val isSongInEnglish: Boolean,
+        val songQuality: Float,
+        val normalizedPoints: Float,
+        val energy: Float,
+        val duration: Float,
+        val acousticness: Float,
+        val danceability: Float,
+        val tempo: Float,
+        val speechines: Float,
+        val key: Int,
+        val liveness: Float,
+        val timeSignature: Int,
+        val mode: Int,
+        val loudness: Float,
+        val valence: Float,
+        val happiness: Float
+){
+    class SongArrayDeserializer: ResponseDeserializable<Array<Song>>{
+        override fun deserialize(content: String): Array<Song>? {
+            return Gson().fromJson(content, Array<Song>::class.java)
+        }
+    }
+}
